8cf6db7d13c5253deada4ac62bb26e454691f39f
trandangquang <trandangquang2203@gmail.com>
2024-04-03 16:14:43 +0700

    update url image

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
-const jsonServer = require("json-server");
-const router = jsonServer.router("db.json");
+const jsonServer = require('json-server');
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = 3001;
 // JSON Server setup
@@ -7,54 +7,60 @@ const server = jsonServer.create();
 server.use(middlewares);
 server.use(
   jsonServer.rewriter({
-    "/api/*": "/$1",
+    '/api/*': '/$1',
   })
 );
 server.use(jsonServer.bodyParser);
 server.use((req, res, next) => {
-  if (req.method === "POST") {
+  if (req.method === 'POST') {
     req.body.createdAt = Date.now();
     req.body.updatedAt = Date.now();
-  } else if (req.method === "PATCH" || req.method === "PUT") {
+  } else if (req.method === 'PATCH' || req.method === 'PUT') {
     req.body.updatedAt = Date.now();
   }
   next();
 });
 
-server.post("/login", (req, res) => {
+server.post('/login', (req, res) => {
   const { email, password } = req.body;
   if (!email || !password) {
-    return res.status(400).json({ message: "Please provide both email and password" });
+    return res
+      .status(400)
+      .json({ message: 'Please provide both email and password' });
   }
-  const users = router.db.get("users").value();
+  const users = router.db.get('users').value();
 
-  const user = users.find(u => u.email === email && u.password === password);
+  const user = users.find((u) => u.email === email && u.password === password);
 
   if (!user) {
-    return res.status(401).json({ message: "Invalid credentials" });
+    return res.status(401).json({ message: 'Invalid credentials' });
   }
 
-  res.json({ message: "Login successful" });
+  res.json({ message: 'Login successful' });
 });
-server.post("/register", (req, res) => {
+server.post('/register', (req, res) => {
   const { email, name, password } = req.body;
   if (!email || !name || !password) {
-    return res.status(400).json({ message: "Please provide email, name, and password." });
+    return res
+      .status(400)
+      .json({ message: 'Please provide email, name, and password.' });
   }
-  const users = router.db.get("users").value();
-  const existingUser = users.find(user => user.email === email);
+  const users = router.db.get('users').value();
+  const existingUser = users.find((user) => user.email === email);
   if (existingUser) {
-    return res.status(400).json({ message: "User already exists with the given email." });
+    return res
+      .status(400)
+      .json({ message: 'User already exists with the given email.' });
   }
   const newUser = { id: users.length + 1, email, name, password };
-  router.db.get("users").push(newUser).write();
-  res.status(201).json({ message: "User registered successfully." });
+  router.db.get('users').push(newUser).write();
+  res.status(201).json({ message: 'User registered successfully.' });
 });
 
-server.get("/locations", (req, res, next) => {
+server.get('/locations', (req, res, next) => {
   const page = parseInt(req.query._page) || 1;
   const perPage = parseInt(req.query._per_page) || 10;
-  let data = router.db.get("Locations").value();
+  let data = router.db.get('Locations').value();
   const totalItems = data.length;
   const totalPages = Math.ceil(totalItems / perPage);
   const startIdx = (page - 1) * perPage;
@@ -71,27 +77,45 @@ server.get("/locations", (req, res, next) => {
     last: lastPage,
     pages: totalPages,
     items: totalItems,
-    data: paginatedData
+    data: paginatedData,
   });
 });
 
-
-server.get("/search/locations", (req, res, next) => {
+server.get('/search/locations', (req, res, next) => {
   const queryParams = req.query;
-  let data = router.db.get("Locations").value();
-  if (Object.keys(queryParams).length > 0) {
-    data = data.filter(course => {
-      return Object.entries(queryParams).every(([key, value]) => {
-        if (Array.isArray(value)) {
-          return value.some(item => course[key].toLowerCase().includes(item.toLowerCase()));
-        } else {
-          return course[key].toLowerCase().includes(value.toLowerCase());
-        }
-      });
-    });
-  }
-  res.json(data);
+  const locationName = queryParams.name ? queryParams.name.toLowerCase() : null;
+  const workplaceName = queryParams['workplace?name']
+    ? queryParams['workplace?name'].toLowerCase()
+    : null;
+
+  let data = router.db.get('Locations').value();
+  let results = [];
+
+  data.forEach((location) => {
+    let locationMatch =
+      !locationName || location.name.toLowerCase().includes(locationName);
+    let filteredWorkplaces = [];
+
+    if (location.Workplaces && locationMatch) {
+      if (workplaceName) {
+        filteredWorkplaces = location.Workplaces.filter((workplace) =>
+          workplace.name.toLowerCase().includes(workplaceName)
+        );
+      } else {
+        filteredWorkplaces = location.Workplaces;
+      }
+      if (filteredWorkplaces.length > 0) {
+        results.push({
+          ...location,
+          Workplaces: filteredWorkplaces,
+        });
+      }
+    }
+  });
+
+  res.json(results);
 });
+
 server.listen(port, () => {
   console.log(`Ecommerce website listening on http://localhost:${port}`);
 });
