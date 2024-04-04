ebdc5ac520fa4308c8a21de9dc7a1c363b8ebdf7
SelinaHoHo <ngocthanhnbk123@gmail.com>
2024-04-04 17:12:44 +0700

    hot fix

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const jsonServer = require("json-server");
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
+const crypto = require("crypto");
+const https = require("https");
 const port = 3001;
 // JSON Server setup
 const server = jsonServer.create();
@@ -136,6 +138,93 @@ server.get("/search/locations", (req, res, next) => {
   });
 
   res.json(results);
+});
+
+server.post("/order", (req, res) => {
+  const { amountNumber, description } = req.body;
+  if (!amountNumber || Number(amountNumber) <= 0) {
+    return res
+      .status(400)
+      .json({ message: "Please provide amount and description" });
+  }
+  var partnerCode = "MOMO";
+  var accessKey = "F8BBA842ECF85";
+  var secretkey = "K951B6PE1waDMi640xX08PD3vg6EkVlz";
+  var requestId = partnerCode + new Date().getTime();
+  var orderId = requestId;
+  var orderInfo = "pay with MoMo";
+  var redirectUrl = "http://localhost:5173/";
+  var ipnUrl = "https://callback.url/notify";
+  var amount = String(amountNumber);
+  var requestType = "captureWallet";
+  var extraData = "";
+  var rawSignature =
+    "accessKey=" +
+    accessKey +
+    "&amount=" +
+    amount +
+    "&extraData=" +
+    extraData +
+    "&ipnUrl=" +
+    ipnUrl +
+    "&orderId=" +
+    orderId +
+    "&orderInfo=" +
+    orderInfo +
+    "&partnerCode=" +
+    partnerCode +
+    "&redirectUrl=" +
+    redirectUrl +
+    "&requestId=" +
+    requestId +
+    "&requestType=" +
+    requestType;
+  var signature = crypto
+    .createHmac("sha256", secretkey)
+    .update(rawSignature)
+    .digest("hex");
+  const requestBody = JSON.stringify({
+    partnerCode: partnerCode,
+    accessKey: accessKey,
+    requestId: requestId,
+    amount: amount,
+    orderId: orderId,
+    orderInfo: orderInfo,
+    redirectUrl: redirectUrl,
+    ipnUrl: ipnUrl,
+    extraData: extraData,
+    requestType: requestType,
+    signature: signature,
+    lang: "en",
+  });
+  const options = {
+    hostname: "test-payment.momo.vn",
+    port: 443,
+    path: "/v2/gateway/api/create",
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Content-Length": Buffer.byteLength(requestBody),
+    },
+  };
+  const reqMomo = https.request(options, (resMomo) => {
+    let data = "";
+    resMomo.on("data", (chunk) => {
+      data += chunk;
+    });
+    resMomo.on("end", () => {
+      console.log("Response from MoMo API:");
+      console.log(data);
+      const responseData = JSON.parse(data);
+      res.json(responseData);
+    });
+  });
+  reqMomo.on("error", (e) => {
+    console.error(`Error sending request to MoMo API: ${e.message}`);
+    res.status(500).json({ error: "Internal Server Error" });
+  });
+  reqMomo.write(requestBody);
+  reqMomo.end();
 });
 
 server.listen(port, () => {
